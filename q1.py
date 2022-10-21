023373dd2cd99f7fa57510f7ccb99e10e9c280d2
mrlatm <aminlatme@gmail.com>
2022-10-21 08:11:33 +0330

    first commit

---
+++ q1.py
@@ -0,0 +1,4 @@
+# print(f"{input('Enter first characte: ')}-{input('Enter second character: ')}-{input('Enter third character: ')}")
+
+print(f"{input('Enter first characte: ')}", f"{input('Enter second character: ')}",
+      f"{input('Enter third character: ')}", sep="-")
