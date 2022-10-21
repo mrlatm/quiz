048b7618a6abc87be60101c2e7492907d3c47db9
amin <77725204+mrlatm@users.noreply.github.com>
2022-10-21 06:59:51 +0330

    add q7 and q8

---
+++ q7.py
@@ -0,0 +1,8 @@
+#    012345678910
+a = "Hello world"
+print(a[0:2], a[1]*5, a[2:], sep="")
+print(a[0:4])
+print(a[0], a[7], a[9], a[10]*6, sep="")
+print(a[4::-1])
+print(a[6:])
+print(a[0:4], a[4]*8, sep="")
